e62bf941422a693ce46664d439de4c6e7217f42a
Suyogya Luitel <lsuyogya@gmail.com>
2025-02-25 14:36:25 +0545

    feat: added menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const body = document.querySelector("body");
-  const header = document.querySelector("header");
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body');
+  const header = document.querySelector('header');
   function bodyStyles() {
-    body.style.setProperty("--headerHeight", header.offsetHeight + "px");
-    body.style.setProperty("--vh", window.innerHeight + "px");
-    body.style.setProperty("--vw", window.innerWidth + "px");
+    body.style.setProperty('--headerHeight', header.offsetHeight + 'px');
+    body.style.setProperty('--vh', window.innerHeight + 'px');
+    body.style.setProperty('--vw', window.innerWidth + 'px');
   }
 
-  document.addEventListener("scroll", () => {
-    const scrollX = window.scrollX + "px";
-    const scrollY = window.scrollY + "px";
+  document.addEventListener('scroll', () => {
+    const scrollX = window.scrollX + 'px';
+    const scrollY = window.scrollY + 'px';
 
-    body.style.setProperty("--scrollX", scrollX);
-    body.style.setProperty("--scrollY", scrollY);
-  });
-
-  const parallexColumns = document.querySelectorAll(".parallexColumns .column");
-  parallexColumns.forEach((column, index) => {
-    const multiplier = (Math.pow(index, 1.5) + 1) / 20;
-    column.style.setProperty("--multiplier", multiplier);
+    body.style.setProperty('--scrollX', scrollX);
+    body.style.setProperty('--scrollY', scrollY);
   });
 
   function txtOpacity() {
     const rootElement = document.querySelector(
-      ".parallexColumns + .txtContent div:has(> p:first-child):has(> p:last-child)"
+      '.parallexColumns + .txtContent div:has(> p:first-child):has(> p:last-child)'
     );
-    const paras = rootElement.querySelectorAll("p");
+    const paras = rootElement.querySelectorAll('p');
 
     paras.forEach((para) => {
-      let htmlString = "";
-      let letterArray = para.textContent.split("");
+      let htmlString = '';
+      let letterArray = para.textContent.split('');
       letterArray.forEach((letter, index) => {
-        if (letter != "") htmlString += `<span>${letterArray[index]}</span>`;
+        if (letter != '') htmlString += `<span>${letterArray[index]}</span>`;
       });
       para.innerHTML = htmlString;
     });
 
-    const spans = rootElement.querySelectorAll("span");
+    const spans = rootElement.querySelectorAll('span');
     //VH calculation, persoanl monitor reference;
     const startPointPx = 2000;
     const endPointPx = 2400;
@@ -50,20 +44,20 @@ document.addEventListener("DOMContentLoaded", () => {
         let { top, left } = span.getBoundingClientRect();
         const scrollRatio = window.scrollY / trueStartPointPx;
         if (scrollRatio < 4) {
-          const closestDiv = span.closest("div");
+          const closestDiv = span.closest('div');
           const closestDivTop = closestDiv.getBoundingClientRect().top;
           top = (top - closestDivTop) / 550;
           left = left / 1000 / 1.5;
           const opacityValue =
             scrollRatio - (top + left) < 0.1 ? 0.1 : scrollRatio - (top + left);
           span.style.opacity = opacityValue;
-          span.style.setProperty("--top", top);
-          span.style.setProperty("--left", left);
-          span.style.setProperty("--trueStartingPoint", trueStartPointPx);
+          span.style.setProperty('--top', top);
+          span.style.setProperty('--left', left);
+          span.style.setProperty('--trueStartingPoint', trueStartPointPx);
         }
       });
     }
-    document.addEventListener("scroll", reveal);
+    document.addEventListener('scroll', reveal);
     reveal();
   }
 
@@ -85,53 +79,78 @@ document.addEventListener("DOMContentLoaded", () => {
     const opacity = maxOpacity * (1 - Math.abs(2 * progress - 1));
 
     // Update opacity directly
-    document.querySelector(".parallexColumns").style.opacity = opacity;
+    document.querySelector('.parallexColumns').style.opacity = opacity;
 
     // Apply additional logic for `.txtContent`
     const txtContentOpacity = Math.max(
       0,
       (scrollY - (105 * viewportHeight) / 100) / 100
     );
-    document.querySelector(".txtContent").style.opacity = txtContentOpacity;
+    document.querySelector('.txtContent').style.opacity = txtContentOpacity;
   }
 
-  window.addEventListener("scroll", updateOpacity);
-  window.addEventListener("resize", updateOpacity); // Handle screen resize
-  window.addEventListener("resize", bodyStyles); // Handle screen resize
-  updateOpacity(); // Initialize on page load
+  if (window.innerWidth >= 1000) {
+    window.addEventListener('scroll', updateOpacity);
+    window.addEventListener('resize', updateOpacity); // Handle screen resize
+    window.addEventListener('resize', bodyStyles); // Handle screen resize
+    updateOpacity(); // Initialize on page load
+    txtOpacity();
+    const parallexColumns = document.querySelectorAll(
+      '.parallexColumns .column'
+    );
+    parallexColumns.forEach((column, index) => {
+      const multiplier = (Math.pow(index, 1.5) + 1) / 20;
+      column.style.setProperty('--multiplier', multiplier); //css will use this to translate, at different speeds
+    });
+  }
   bodyStyles(); // Initialize on page load
 
-  var splide = new Splide("#splideProjectsLeft", {
+  var splideProjectsLeft = new Splide('#splideProjectsLeft', {
     arrows: false,
     pagination: false,
     perPage: 1,
     perSlide: 1,
   });
-  var splide2 = new Splide("#splideProjectsRight", {
+  var splideProjectsRight = new Splide('#splideProjectsRight', {
     arrows: false,
     pagination: false,
     perPage: 1,
     perSlide: 1,
   });
-  splide.sync(splide2);
-  splide.mount();
-  splide2.mount();
+  splideProjectsLeft.sync(splideProjectsRight);
+  splideProjectsLeft.mount();
+  splideProjectsRight.mount();
 
-  const slideBtns = document.querySelectorAll(".projectSlideBtn");
+  const slideBtns = document.querySelectorAll('.projectSlideBtn');
   slideBtns.forEach((slideBtn) => {
-    const attr = slideBtn.getAttribute("data-page");
-    if (attr == "next") {
-      slideBtn.addEventListener("click", () => {
-        splide.go(">");
+    const attr = slideBtn.getAttribute('data-page');
+    if (attr == 'next') {
+      slideBtn.addEventListener('click', () => {
+        splideProjectsLeft.go('>');
       });
     } else {
-      slideBtn.addEventListener("click", () => {
-        splide.go("<");
+      slideBtn.addEventListener('click', () => {
+        splideProjectsLeft.go('<');
       });
     }
   });
 
   feather.replace();
 
-  txtOpacity();
+  // Menu Gap...not needed anymore
+  // const menuAnchors = document.querySelectorAll('.menuNav>a');
+  // menuAnchors.forEach((menuAnchor, index) => {
+  //   if (index % 2 != 0) index = index - 1;
+  //   menuAnchor.style.setProperty('--order', index);
+  // });
+
+  //Menu
+  const openBtn = document.querySelector('.hamburger');
+  const closeBtn = document.querySelector('.menuOverlay .close');
+  openBtn.addEventListener('click', () => {
+    body.setAttribute('data-menu', 'open');
+  });
+  closeBtn.addEventListener('click', () => {
+    body.setAttribute('data-menu', 'close');
+  });
 });
