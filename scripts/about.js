686073fc1e58428adcc3067fe3ddfe773abeab74
Suyogya Luitel <lsuyogya@gmail.com>
2025-02-28 17:01:38 +0545

    feat: services init

--- scripts/about.js
+++ scripts/about.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   if (!Splide) return;
-  const slider = new Splide('#coreValueSlider', {
+  const sliderRoot = document.querySelector('#coreValueSlider');
+  if (!sliderRoot) return;
+  const slider = new Splide(sliderRoot, {
     arrows: false,
     pagination: true,
     perPage: 1,
@@ -10,4 +12,24 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
   slider.mount();
+});
+document.addEventListener('DOMContentLoaded', () => {
+  if (!Splide) return;
+  const sliderRoots = document.querySelectorAll('.serviceSplide');
+  if (!sliderRoots) return;
+  sliderRoots.forEach((sliderRoot) => {
+    const slider = new Splide(sliderRoot, {
+      arrows: false,
+      pagination: true,
+      perPage: 1,
+      perSlide: 1,
+      autoplay: true,
+      interval: 3000,
+      type: 'loop',
+      classes: {
+        page: 'splide__pagination__page page-rounded-rect',
+      },
+    });
+    slider.mount();
+  });
 });
