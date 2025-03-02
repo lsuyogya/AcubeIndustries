9eb9b5ac1051c40d0ce8b3fc118c85d22734f194
Suyogya Luitel <Lsuyogya@gmail.com>
2025-03-02 10:49:48 +0545

    feat: services minor: feedbacks

--- scripts/about.js
+++ scripts/about.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   if (!Splide) return;
-  const sliderRoot = document.querySelector('#coreValueSlider');
+  const sliderRoot = document.querySelector("#coreValueSlider");
   if (!sliderRoot) return;
   const slider = new Splide(sliderRoot, {
     arrows: false,
@@ -8,14 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
     perPage: 1,
     perSlide: 1,
     classes: {
-      page: 'splide__pagination__page page-rounded-rect',
+      page: "splide__pagination__page page-rounded-rect",
     },
   });
   slider.mount();
 });
-document.addEventListener('DOMContentLoaded', () => {
+
+document.addEventListener("DOMContentLoaded", () => {
   if (!Splide) return;
-  const sliderRoots = document.querySelectorAll('.serviceSplide');
+  const sliderRoots = document.querySelectorAll(".serviceSplide");
   if (!sliderRoots) return;
   sliderRoots.forEach((sliderRoot) => {
     const slider = new Splide(sliderRoot, {
@@ -25,9 +26,9 @@ document.addEventListener('DOMContentLoaded', () => {
       perSlide: 1,
       autoplay: true,
       interval: 3000,
-      type: 'loop',
+      type: "loop",
       classes: {
-        page: 'splide__pagination__page page-rounded-rect',
+        page: "splide__pagination__page page-rounded-rect",
       },
     });
     slider.mount();
