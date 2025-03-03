90eae51e9491dd83dc8431e442fde267b40f0ae2
Suyogya Luitel <lsuyogya@gmail.com>
2025-03-03 16:46:38 +0545

    minor: feedbacks

--- scripts/about.js
+++ scripts/about.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   if (!Splide) return;
-  const sliderRoot = document.querySelector("#coreValueSlider");
+  const sliderRoot = document.querySelector('#coreValueSlider');
   if (!sliderRoot) return;
   const slider = new Splide(sliderRoot, {
     arrows: false,
@@ -8,15 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
     perPage: 1,
     perSlide: 1,
     classes: {
-      page: "splide__pagination__page page-rounded-rect",
+      page: 'splide__pagination__page page-rounded-rect',
     },
   });
   slider.mount();
 });
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   if (!Splide) return;
-  const sliderRoots = document.querySelectorAll(".serviceSplide");
+  const sliderRoots = document.querySelectorAll('.serviceSplide');
   if (!sliderRoots) return;
   sliderRoots.forEach((sliderRoot) => {
     const slider = new Splide(sliderRoot, {
@@ -26,11 +26,31 @@ document.addEventListener("DOMContentLoaded", () => {
       perSlide: 1,
       autoplay: true,
       interval: 3000,
-      type: "loop",
+      type: 'loop',
       classes: {
-        page: "splide__pagination__page page-rounded-rect",
+        page: 'splide__pagination__page page-rounded-rect',
       },
     });
     slider.mount();
   });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (!Splide) return;
+  const sliderRoot = document.querySelector('#projectDetail');
+  if (!sliderRoot) return;
+
+  const slider = new Splide(sliderRoot, {
+    arrows: false,
+    pagination: true,
+    perPage: 1,
+    perSlide: 1,
+    autoplay: true,
+    interval: 3000,
+    type: 'loop',
+    classes: {
+      page: 'splide__pagination__page page-rounded-rect',
+    },
+  });
+  slider.mount();
 });
